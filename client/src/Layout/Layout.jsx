f3db2a6f969be7f930a9d83a1bd82878fb0343d1
Ialbertine <a.ingabire1@alustudent.com>
2024-08-20 22:43:45 +0200

    Layout updates

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Components/Header";
 import { Outlet } from "react-router-dom";
-import PreFooter from "../Components/PreFooter";
+import PreFooter from "../Components/Prefooter";
 import Footer from "../Components/Footer";
 
 const Layout = () => {
